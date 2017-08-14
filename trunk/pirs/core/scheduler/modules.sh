db091af7e5cb50a6f96460dea9e2f44265a997d0
travleev <anton.travleev@kit.edu>
2017-08-15 01:18:54 +0200

    Added cd to PBS_O_WORKDIR and list of modules for mpi4py.

--- trunk/pirs/core/scheduler/modules.sh
+++ trunk/pirs/core/scheduler/modules.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=5:mpiprocs=5
-#PBS -l walltime=00:05:00
-#PBS -A FUA11_MCHIFI
-#PBS -q xfuadebug
-
-cd $PBS_O_WORKDIR
+# Modules must be loaded in specific order:
 
 module load python/2.7.12                
 module load intel/pe-xe-2017--binary     
@@ -13,6 +8,3 @@ module load intelmpi/2017--binary
 module load mkl/2017--binary             
 module load numpy/1.11.2--python--2.7.12 
 module load mpi4py/2.0.0--python--2.7.12 
-
-mpirun python workplaces.py 
-
