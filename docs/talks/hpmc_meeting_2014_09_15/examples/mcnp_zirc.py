79f3055f4ed90c8d2747f2294b916c1d86410cca
travleev <anton.travleev@kit.edu>
2018-02-19 05:36:12 +0100

    Added copyright note to all py files.

--- docs/talks/hpmc_meeting_2014_09_15/examples/mcnp_zirc.py
+++ docs/talks/hpmc_meeting_2014_09_15/examples/mcnp_zirc.py
@@ -1,4 +1,21 @@
 from pirs.mcnp import Material
+# Copyright 2015 Karlsruhe Institute of Technology (KIT)
+#
+# This file is part of PIRS-2.
+#
+# PIRS-2 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PIRS-2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # zircaloy-2, Table 5, p.7
 zr = Material(('Zr', 98.23), 
               ('Sn', 1.50),
--- examples/pwr_bench/temp_comapre.py
+++ examples/pwr_bench/temp_comapre.py
@@ -1,4 +1,21 @@
 # coolant temperature is specified in 'results for channel' tables
+# Copyright 2015 Karlsruhe Institute of Technology (KIT)
+#
+# This file is part of PIRS-2.
+#
+# PIRS-2 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PIRS-2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # and in 'results for rod' tables. Here I compare them for a_scf_1 case.
 chT = {}
 chR = {}
